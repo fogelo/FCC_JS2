537b243cb1b8554b769e57ec383146dfcfe3f543
antonorlov <fogelo@yandex.ru>
2022-01-05 19:03:09 +0300

    Generate Random Whole Numbers with JavaScript2

--- script.js
+++ script.js
@@ -247,6 +247,11 @@ function randomWholeNum() {
     return Math.floor(Math.random()*10);
 }
 
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+}
 
-// const result = randomWholeNum()
-// console.log(result)
+const result = randomRange()
+console.log(result)
