4aea5c2341c9dcda565ad67fbc9e827f355ff119
antonorlov <fogelo@yandex.ru>
2022-01-11 14:44:50 +0300

    Use Destructuring Assignment to Assign Variables from Arrays

--- script.js
+++ script.js
@@ -423,16 +423,21 @@ console.log(today)*/
 // nested - вложенный
 
 const LOCAL_FORECAST = {
-    yesterday: { low: 61, high: 75 },
-    today: { low: 64, high: 77 },
-    tomorrow: { low: 68, high: 80 }
+    yesterday: {low: 61, high: 75},
+    today: {low: 64, high: 77},
+    tomorrow: {low: 68, high: 80}
 };
 
 
-const {today: {low: lowToday,high: highToday}} = LOCAL_FORECAST
+const {today: {low: lowToday, high: highToday}} = LOCAL_FORECAST
 console.log(lowToday)
 
+/*§28. Use Destructuring Assignment to Assign Variables from Arrays*/
 
+let a = 8, b = 6;
+// Only change code below this line
+
+[a, b] = [b, a]
 
 
 
