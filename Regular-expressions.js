462a6bfaff02a04f1d95d96ed78b56c46cba210e
antonorlov <fogelo@yandex.ru>
2022-01-11 20:33:49 +0300

    Match Letters of the Alphabet

--- Regular-expressions.js
+++ Regular-expressions.js
@@ -28,12 +28,28 @@ console.log(result)*/
 
 /*?5. Find More Than the First Match*/
 
-let twinkleStar = "Twinkle, twinkle, little star";
+/*let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
-console.log(result)
+console.log(result)*/
 
 /*?6. Match Anything with Wildcard Period*/
+/*
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result = unRegex.test(exampleStr);
+*/
+
+/*?7. Match Single Character with Multiple Possibilities*/
+
+/*let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+console.log(result)*/
+
+/*?8. Match Letters of the Alphabet*/
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+console.log(result)
