6cc1a505750f3ed43302987ec5ea33ab6900875b
antonorlov <fogelo@yandex.ru>
2022-01-11 19:11:21 +0300

    Match a Literal String with Different Possibilities

--- Regular-expressions.js
+++ Regular-expressions.js
@@ -1,7 +1,13 @@
 /*?1. Using the Test Method*/
 
 //метод test тестирует строку на наличие в ней совпадения с регулярным выражением (вернет true или false)
-let myString = "Hello, World!";
+/*let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
-console.log(result)
+console.log(result)*/
+
+/*?2. Match a Literal String with Different Possibilities*/
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
