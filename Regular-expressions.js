cfabc72c7f95e0dae2354f6e13c2a4b06ad7c0e9
antonorlov <fogelo@yandex.ru>
2022-01-11 19:12:45 +0300

    Ignore Case While Matching

--- Regular-expressions.js
+++ Regular-expressions.js
@@ -8,6 +8,13 @@ console.log(result)*/
 
 /*?2. Match a Literal String with Different Possibilities*/
 
+/*
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
-let result = petRegex.test(petString);
+let result = petRegex.test(petString);*/
+
+/*?3. Ignore Case While Matching*/
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
