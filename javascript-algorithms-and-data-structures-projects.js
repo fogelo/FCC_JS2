02171bb10a83018de330bcf804fd3b5a5d7af0f1
antonorlov <fogelo@yandex.ru>
2022-01-17 15:59:18 +0300

    Caesars Cipher

--- javascript-algorithms-and-data-structures-projects.js
+++ javascript-algorithms-and-data-structures-projects.js
@@ -26,8 +26,7 @@ const result = palindrome("race car");
 console.log(result)*/
 
 /*?2. Roman Numeral Converter*/
-function convertToRoman(num) {
-    console.log(num)
+/*function convertToRoman(num) {
     let obj = {
         1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X',
         20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC', 100: 'C',
@@ -35,7 +34,6 @@ function convertToRoman(num) {
         2000: 'MM', 3000: 'MMM',
     }
     let arr = num.toString().split('').reverse()
-    console.log(arr)
     for (let i = 0; i < arr.length; i++) {
         arr[i] !== '0' ? arr[i] = arr[i] + '0'.repeat(i) : arr[i] = ''
     }
@@ -44,6 +42,23 @@ function convertToRoman(num) {
         .replace(/\d+/g, match => obj[match])
         .replace(/\s/g, '')
 }
-
 const result = convertToRoman(501);
+console.log(result)*/
+
+/*?3. Caesars Cipher*/
+
+function rot13(str) {
+    return [...str]
+        .map(item => item.charCodeAt(0) - 13)
+        .map(item => item === 19 || item === 50 || item === 33 || item === 20 ? item + 13
+            : item < 65 ? 90 - 64 + item : item)
+        .map(item => String.fromCharCode(item))
+        .join('')
+}
+
+const result = rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT?.!");
 console.log(result)
+console.log('!'.charCodeAt())
+console.log('.'.charCodeAt())
+
+// item !== ' ' && item !== '!' && item !== '?' && item !== '.' ?
