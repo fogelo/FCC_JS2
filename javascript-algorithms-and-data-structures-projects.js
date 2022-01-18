b3742edbdb4f46065933c7c4c0bb43542c3c2693
antonorlov <fogelo@yandex.ru>
2022-01-18 18:45:31 +0300

    Cash Register

--- javascript-algorithms-and-data-structures-projects.js
+++ javascript-algorithms-and-data-structures-projects.js
@@ -61,6 +61,7 @@ const result = rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT?.!");*/
 
 /*?4. Telephone Number Validator*/
 
+/*
 function telephoneCheck(str) {
     let regExp = /^1?\s?(\(\d{3}\)|\d{3})-?\s?\d{3}-?\s?\d{4}$/g
     return regExp.test(str)
@@ -68,13 +69,209 @@ function telephoneCheck(str) {
 
 const result = telephoneCheck("(275)76227382");
 console.log(result)
+*/
 
+
+/*§5. Cash Register*/
+//@ мое решение до рефакторинга
+/*function checkCashRegister(price, cash, cid) {
+    let change = []
+    let differ = cash - price
+    let allMoney = cid.reduce((prev, item) => prev + item[1], 0)
+
+    if (allMoney < differ) {
+        return {status: "INSUFFICIENT_FUNDS", change: change}
+    }
+    if (allMoney === differ) {
+        return {status: "CLOSED", change: [...cid]}
+    }
+    if (differ < 100) {
+        change.push(["ONE HUNDRED", 0])
+    }
+    if (differ > 100) {
+        if (differ / 100 > cid[8][1] / 100) {
+            change.push(["ONE HUNDRED", cid[8][1]])
+            differ = differ - cid[8][1]
+        } else {//differ / 100 < cid[8][1] / 100)
+            change.push(["ONE HUNDRED", Math.trunc(differ / 100) * 100])
+            differ = differ % 100
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    console.log(differ)
+    if (differ < 20) {
+        change.push(["TWENTY", 0])
+    }
+    if (differ > 20) {
+        if (differ / 20 > cid[7][1] / 20) {
+            change.push(["TWENTY", cid[7][1]])
+            differ = differ - cid[7][1]
+        } else {
+            change.push(["TWENTY", Math.trunc(differ / 20) * 20])
+            differ = differ % 20
+        }
+    }
+    console.log(differ)
+
+    differ = Math.round(differ*100)/100
+    console.log(differ)
+    if (differ < 10) {
+        change.push(["TEN", 0])
+    }
+    if (differ > 10) {
+        if (differ / 10 > cid[6][1] / 10) {
+            change.push(["TEN", cid[6][1]])
+            differ = differ - cid[6][1]
+        } else {
+            change.push(["TEN", Math.trunc(differ / 10) * 10])
+            differ = differ % 20
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 5) {
+        change.push(["FIVE", 0])
+    }
+    if (differ > 5) {
+        if (differ / 10 > cid[5][1] / 5) {
+            change.push(["FIVE", cid[5][1]])
+            differ = differ - cid[5][1]
+        } else {
+            change.push(["FIVE", Math.trunc(differ / 5) * 5])
+            differ = differ % 5
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 1) {
+        change.push(["ONE", 0])
+    }
+    if (differ > 1) {
+        if (differ / 1 > cid[4][1] / 1) {
+            change.push(["ONE", cid[4][1]])
+            differ = differ - cid[4][1]
+        } else {
+            change.push(["ONE", Math.trunc(differ / 1) * 1])
+            differ = differ % 1
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 0.25) {
+        change.push(["QUARTER", 0])
+    }
+    if (differ > 0.25) {
+        if (differ / 0.25 > cid[3][1] / 0.25) {
+            change.push(["QUARTER", cid[3][1]])
+            differ = differ - cid[3][1]
+        } else {
+            change.push(["QUARTER", Math.trunc(differ / 0.25) * 0.25])
+            differ = differ % 0.25
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 0.1) {
+        change.push(["DIME", 0])
+    }
+    if (differ > 0.1) {
+        if (differ / 0.1 > cid[2][1] / 0.1) {
+            change.push(["DIME", cid[2][1]])
+            differ = differ - cid[2][1]
+        } else {
+            change.push(["DIME", Math.trunc(differ / 0.1) * 0.1])
+            differ = differ % 0.1
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 0.05) {
+        change.push(["NICKEL", 0])
+    }
+    if (differ > 0.05) {
+        if (differ / 0.05 > cid[1][1] / 0.05) {
+            change.push(["NICKEL", cid[1][1]])
+            differ = differ - cid[1][1]
+        } else {
+            change.push(["NICKEL", Math.trunc(differ / 0.05) * 0.05])
+            differ = differ % 0.05
+        }
+    }
+    differ = Math.round(differ*100)/100
+
+    if (differ < 0.01) {
+        change.push(["PENNY", 0])
+    }
+    if (differ > 0.01) {
+        if (differ / 0.01 > cid[0][1] / 0.01) {
+            change.push(["PENNY", cid[0][1]])
+            differ = differ - cid[0][1]
+        } else {
+            change.push(["PENNY", Math.trunc(differ / 0.01) * 0.01])
+            differ = differ % 0.01
+        }
+    }
+    console.log(differ)
+    console.log(change)
+    let suitableChange = change.reduce((prev, item) => prev + item[1], 0)
+
+        if(suitableChange<(cash-price)){
+           return  {status: "INSUFFICIENT_FUNDS", change: []}
+        }
+    return {status: "OPEN", change: change.filter(item => item[1] !== 0)}
+}*/
+
+//@ мое решение после рефакторинга
+function checkCashRegister(price, cash, cid) {
+    let change = []
+    let differ = cash - price
+    let allMoney = cid.reduce((prev, item) => prev + item[1], 0)
+    let billDenominations = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
+
+    if (allMoney < differ) {
+        return {status: "INSUFFICIENT_FUNDS", change: change}
+    }
+    if (allMoney === differ) {
+        return {status: "CLOSED", change: [...cid]}
+    }
+
+    for (let i = 8; i >= 0; i--) {
+        if (differ < billDenominations[i]) {
+            change.push([cid[i][0], 0])
+        }
+        if (differ > billDenominations[i]) {
+            if (differ / billDenominations[i] > cid[i][1] / billDenominations[i]) {
+                change.push([cid[i][0], cid[i][1]])
+                differ = differ - cid[i][1]
+            } else {
+                change.push([cid[i][0], Math.trunc(differ / billDenominations[i]) * billDenominations[i]])
+                differ = differ % billDenominations[i]
+            }
+        }
+        differ = Math.round(differ * 100) / 100
+    }
+
+    let suitableChange = change.reduce((prev, item) => prev + item[1], 0)
+    if (suitableChange < (cash - price)) {
+        return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+    return {status: "OPEN", change: change.filter(item => item[1] !== 0)}
+}
+
+
+const result = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55],
+    ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+console.log(result)
 
 /*
-555-555-5555
-(555)555-5555
-(555) 555-5555
-555 555 5555
-5555555555
-1 555 555 5555
+$0.01 (PENNY)
+$0.05 (NICKEL)
+$0.1 (DIME)
+$0.25 (QUARTER)
+$1 (ONE)
+$5 (FIVE)
+$10 (TEN)
+$20 (TWENTY)
+$100 (ONE HUNDRED)
 */
